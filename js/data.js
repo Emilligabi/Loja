7cf87c7c1b44d2f32adc5f4c231f89ffb579d276
Emilligabi <103665496+Emilligabi@users.noreply.github.com>
2022-06-30 21:38:16 -0300

    chore: continue project

---
+++ js/data.js
@@ -0,0 +1,27 @@
+const bannerImages = [
+  {
+    id: 1,
+    image_url: "/imagens/banner/banner-a.webp",
+    description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+  },
+  {
+    id: 2,
+    image_url: "/imagens/banner/banner-b.webp",
+    description: "Contrary to popular belief, Lorem Ipsum is not simply random text."
+  },
+  {
+    id: 3,
+    image_url: "/imagens/banner/banner-c.webp",
+    description: "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters."
+  },
+  {
+    id: 4,
+    image_url: "/imagens/banner/banner-d.webp",
+    description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form."
+  },
+  {
+    id: 5,
+    image_url: "/imagens/banner/banner-e.webp",
+    description: "Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  }
+]
