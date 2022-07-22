83e7a89103404197ea08f3ff6e62f6ec3ce9c0b6
Emilli Rodrigues <emillijogo2019@gmail.com>
2022-07-21 21:38:36 -0300

    loja

--- js/page-cart.js
+++ js/page-cart.js
@@ -1,34 +1,47 @@
 class PageClient {
-    constructor() {
-        this.elementTarget = document.getElementById("products-list");
-        this.init()
-    
-        const target = document.getElementById("loading");
-        "loading"
-        .split("")
-        .map((p) => {
-            const s = document.createElement("span");
-            s.innerText = p.toUpperCase();
-            return s;
-    })
+  constructor() {
+    this.elementTarget = document.getElementById("products-list");
+    this.init();
 
-    .forEach((s) => target.append(s));
+    const target = document.getElementById("loading");
+    "loading"
+      .split("")
+      .map((p) => {
+        const s = document.createElement("span");
+        s.innerText = p.toUpperCase();
+        return s;
+      })
+
+      .forEach((s) => target.append(s));
 
     document.getElementById("go-to-register").onclick = () => {
-        document.getElementById("cart").style.display = "none";
-        document.getElementById("register").style.display = "flex";
+      document.getElementById("cart").style.display = "none";
+      document.getElementById("register").style.display = "flex";
     };
-    }
 
-    init() {
+    document.getElementById("client").onsubmit = (event) => {
+      event.preventDefault();
+      const value = document.getElementById("name").value.trim();
 
-    }
+      if (!value) {
+        alert("Preencha o nome corretamente!");
+        return;
+      }
 
-    render(products) {
+      target.classList.add("active");
 
-    }
+      localStorage.removeItem("cart");
+      setTimeout(() => {
+          alert("compra efetuada com sucesso!");
+          location.href = "/";
+      }, 3000);
 
+    };
+  }
 
+  init() {}
+
+  render(products) {}
 }
 
 const pageClient = new PageClient();
